cecabbe9ef0946debf1642057f783f864df35508
pyagmyrov <permanyagmyrov@gmail.com>
2020-05-29 20:01:13 +0300

    updated

--- retweet.py
+++ retweet.py
@@ -11,6 +11,7 @@ auth = tweepy.OAuthHandler(API_KEY,API_SECRET)
 auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
 api = tweepy.API(auth)
 
+
 #tweet_list = api.favorites(screen_name='demirgazyk',count=5)
 #print(tweet_list)
 

aa7059ad3bb7c6f68de472b860255457c7a629f9
pyagmyrov <permanyagmyrov@gmail.com>
2020-05-29 17:36:20 +0300

    first commit

---
+++ venv/retweet.py
@@ -0,0 +1,29 @@
+import tweepy
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+API_SECRET = os.getenv('API_SECRET')
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+ACCESS_SECRET = os.getenv('ACCESS_SECRET')
+auth = tweepy.OAuthHandler(API_KEY,API_SECRET)
+auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
+api = tweepy.API(auth)
+
+#tweet_list = api.favorites(screen_name='demirgazyk',count=5)
+#print(tweet_list)
+
+#print the id of each tweet
+# for tweet in tweet_list:
+#     print(tweet.id)
+
+# retweet a id
+#tweet_id = 1265736575415132160
+#api.retweet(tweet_id)
+
+for tweet in api.favorites(screen_name='demirgazyk'):
+    try:
+        api.retweet(tweet.id)
+    except tweepy.TweepError as e:
+        print(e)
